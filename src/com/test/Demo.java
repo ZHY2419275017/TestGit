82cd6f61dff87ea4bd653240ef6931ab11cc10f6
zhaohuiyu <2419275017@qq.com>
2019-07-24 19:49:03 +0800

    BigDecimal测试

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 
 public class Demo {
@@ -9,7 +10,7 @@ public class Demo {
 		// System.out.println("第二次更改");
 		// System.out.println(System.currentTimeMillis());
 
-		java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
+		/*java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
 		System.out.println("sqlDate:" + sqlDate);
 		System.out.println(System.currentTimeMillis());
 		java.util.Date utilDate = new java.util.Date(sqlDate.getTime());
@@ -19,7 +20,22 @@ public class Demo {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String strUtilDate = sdf.format(sqlDate);
-		System.out.println("strUtilDate:" + strUtilDate);
+		System.out.println("strUtilDate:" + strUtilDate);*/
+		
+		double d1= 0.02;
+		double d2= 0.03;
+		System.out.println(d2+d1);
+		
+		BigDecimal money1 = new BigDecimal("0.03");
+		BigDecimal money2 = new BigDecimal("0.04");
+		System.out.println(money1.add(money2));
+		System.out.println(money1.subtract(money2));
+		System.out.println(money1.divide(money2));
+		System.out.println(money1.multiply(money2));
+		System.out.println(BigDecimal.ONE);
+		System.out.println(money1.compareTo(BigDecimal.ONE)==0);
+		System.out.println(money1.setScale(2, BigDecimal.ROUND_HALF_UP));
+		
 
 	}
 
