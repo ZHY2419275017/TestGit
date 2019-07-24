b048e178ebb4314d716b9f2238572cca202df54e
zhaohuiyu <2419275017@qq.com>
2019-07-24 13:58:33 +0800

    格式化

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -5,21 +5,21 @@ import java.text.SimpleDateFormat;
 public class Demo {
 
 	public static void main(String[] args) {
-//		System.out.println("��һ�θ���");
-//		System.out.println("第二次更改");
-//		System.out.println(System.currentTimeMillis());
-		
+		// System.out.println("��һ�θ���");
+		// System.out.println("第二次更改");
+		// System.out.println(System.currentTimeMillis());
+
 		java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
-		System.out.println("sqlDate:"+sqlDate);
+		System.out.println("sqlDate:" + sqlDate);
 		System.out.println(System.currentTimeMillis());
 		java.util.Date utilDate = new java.util.Date(sqlDate.getTime());
-		System.out.println("utilDate:"+utilDate);
+		System.out.println("utilDate:" + utilDate);
 		java.sql.Date utilToSqlDate = new java.sql.Date(utilDate.getTime());
-		System.out.println("utilToSqlDate:"+utilToSqlDate);
-		
-	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		System.out.println("utilToSqlDate:" + utilToSqlDate);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String strUtilDate = sdf.format(sqlDate);
-	    System.out.println("strUtilDate:"+strUtilDate);
+		System.out.println("strUtilDate:" + strUtilDate);
 
 	}
 
