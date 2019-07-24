3f71f0cd9b055dbe202a40c66f07446104e1b5b8
zhaohuiyu <2419275017@qq.com>
2019-07-24 09:42:22 +0800

    更改

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -1,10 +1,25 @@
 package com.test;
 
+import java.text.SimpleDateFormat;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("��һ�θ���");
-		System.out.println("第二次更改");
+//		System.out.println("��һ�θ���");
+//		System.out.println("第二次更改");
+//		System.out.println(System.currentTimeMillis());
+		
+		java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
+		System.out.println("sqlDate:"+sqlDate);
+		System.out.println(System.currentTimeMillis());
+		java.util.Date utilDate = new java.util.Date(sqlDate.getTime());
+		System.out.println("utilDate:"+utilDate);
+		java.sql.Date utilToSqlDate = new java.sql.Date(utilDate.getTime());
+		System.out.println("utilToSqlDate:"+utilToSqlDate);
+		
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String strUtilDate = sdf.format(sqlDate);
+	    System.out.println("strUtilDate:"+strUtilDate);
 
 	}
 
